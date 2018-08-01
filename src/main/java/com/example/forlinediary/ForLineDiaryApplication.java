cc3044c1db2c2d2879b26a1398b41e1ca9cbbbc2
Naoaki Hashimoto <hasimoto0130gt@gmail.com>
2018-08-01 21:39:35 +0900

    first

---
+++ src/main/java/com/example/forlinediary/ForLineDiaryApplication.java
@@ -0,0 +1,12 @@
+package com.example.forlinediary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForLineDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForLineDiaryApplication.class, args);
+	}
+}
