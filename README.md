520a3c0d485290e9bf5fbe5a2e9569a87199c985
Naoaki Hashimoto <hasimoto0130gt@gmail.com>
2018-08-10 11:04:50 +0900

    get paper revision

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # for-line-diary
-- https://github.com/dropbox/dropbox-sdk-java#dropbox-for-java-tutorial
+- https://github.com/dropbox/dropbox-sdk-java
+- https://www.dropbox.com/developers/documentation/http/documentation#paper-docs-update
--- src/main/java/com/example/forlinediary/PaperDao.kt
+++ src/main/java/com/example/forlinediary/PaperDao.kt
@@ -1,9 +1,12 @@
 package com.example.forlinediary
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import java.io.BufferedInputStream
 import java.io.InputStream
 import java.net.URL
 import javax.net.ssl.HttpsURLConnection
+
+var revision = "0"
 
 fun httpDonwload(url: String, accessToken: String, apiArgs: String): InputStream? {
 
@@ -21,6 +24,9 @@ fun httpDonwload(url: String, accessToken: String, apiArgs: String): InputStream
     con.connect()
 
     if (con.responseCode in 200..299) {
+        var dropboxApiResult = con.headerFields.get("Dropbox-Api-Result").toString()
+        revision = jacksonObjectMapper().readTree(dropboxApiResult).get(0).get("revision").toString()
+        println(revision)
         return BufferedInputStream(con.inputStream)
     }
 
